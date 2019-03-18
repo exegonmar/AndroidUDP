fa3e51cf796000e8e31deae32e5902f54e9b56e9
Rohail <rohailonline@hotmail.com>
2019-03-18 22:51:17 +0100

    changes

--- app/src/main/java/com/rohail/androidudp/activities/UDPActivity.kt
+++ app/src/main/java/com/rohail/androidudp/activities/UDPActivity.kt
@@ -1,38 +1,39 @@
 package com.rohail.androidudp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.TextUtils
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
 import android.text.method.ScrollingMovementMethod
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.rohail.androidudp.interfaces.MsgCallback
 import com.rohail.androidudp.interfaces.NetworkIPInterface
 import com.rohail.androidudp.network.Client
 import com.rohail.androidudp.network.Server
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class UDPActivity : AppCompatActivity(), View.OnClickListener, MsgCallback, NetworkIPInterface {
 
-    private val sequenceString = arrayOf(
-        "start video1\n" +
-                "    (a 500 ms pause)\n" +
-                "    stop video1\n" +
-                "    start video2\n" +
-                "    (a 1200 ms pause)\n" +
-                "    start fade\n" +
-                "    (a 200 ms pause)\n" +
-                "    stop fade\n" +
-                "    stop video2",
-        "start video2\n" +
-                "    (a 500 ms pause)\n" +
+    private var sequenceString1 = arrayOf(
+        "start video1\n",
+        "    (a {0} ms pause)\n",
+        "    stop video1\n" +
+                "    start video2\n",
+        "    (a {0} ms pause)\n",
+        "    start fade\n",
+        "    (a {0} ms pause)\n",
+        "    stop fade\n" +
+                "    stop video2"
+        /*"start video2\n" +
+                "    (a {0} ms pause)\n" +
                 "    stop video2\n" +
                 "    start video1\n" +
-                "    (a 1200 ms pause)\n" +
+                "    (a {0} ms pause)\n" +
                 "    start fade\n" +
-                "    (a 200 ms pause)\n" +
+                "    (a {0} ms pause)\n" +
                 "    stop fade\n" +
-                "    stop video1"
+                "    stop video1"*/
     )
     private lateinit var client: Client
     private lateinit var server: Server
@@ -41,7 +42,7 @@ class UDPActivity : AppCompatActivity(), View.OnClickListener, MsgCallback, Netw
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.rohail.androidudp.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         var ipExtra: String = intent.getStringExtra("keyIdentifier")
 
@@ -71,15 +72,15 @@ class UDPActivity : AppCompatActivity(), View.OnClickListener, MsgCallback, Netw
 
         when (v!!.id) {
             btnVideo1.id -> {
-                sendMessage(0)
+                sendMessage(0, etVideo1.text.toString())
             }
             btnVideo2.id -> {
-                sendMessage(1)
+                sendMessage(1, etVideo2.text.toString())
             }
         }
     }
 
-    private fun sendMessage(seqNumber: Int) {
+    private fun sendMessage(seqNumber: Int, milliseconds: String) {
         client.stop()
 
         if (thread != null)
@@ -98,13 +99,23 @@ class UDPActivity : AppCompatActivity(), View.OnClickListener, MsgCallback, Netw
     }
 
     private fun getSequence(seqNumber: Int): String {
-        return sequenceString[seqNumber]
+        return seqNumber.toString()
     }
 
     override fun onMsgReceived(msg: String) {
+        val handler = Handler()
+        val milliseconds: Long = etVideo1.text.toString().toLong()
+
         runOnUiThread {
             // Stuff that updates the UI
-            tvDetail.setText(tvDetail.text.toString() + "\n\n\n" + msg)
+            for (message in sequenceString1) {
+                if (message.contains("{0}")) {
+                    message = message.replace("{0}", milliseconds.toString())
+                }
+                handler.postDelayed(Runnable {
+                    tvDetail.setText(tvDetail.text.toString() + "\n\n\n" + message)
+                }, milliseconds)
+            }
         }
     }
 
